754a9b1eeef0226b6178b3157eec3461fa2baa71
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 09:25:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { notificationsRoute } from "./routes/notifications.js";
+import { templatesRoute } from "./routes/templates.js";
+import { historyRoute } from "./routes/history.js";
+
 console.log("NotifyHub started");
+console.log(notificationsRoute());
+console.log(templatesRoute());
+console.log(historyRoute());
