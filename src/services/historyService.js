27994c3e353137101a8a3f4c411ef85ea65f08d2
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 16:55:20 +0300

    Update historyService.js

--- src/services/historyService.js
+++ src/services/historyService.js
@@ -2,6 +2,13 @@ import { buildHistoryRecord } from "../utils/buildHistoryRecord.js";
 
 export function historyService(item = {}) {
   return {
-    items: [buildHistoryRecord(item)],
+    items: [
+      buildHistoryRecord(item),
+      buildHistoryRecord({
+        id: "seed-history-2",
+        channel: "sms",
+        status: "failed",
+      }),
+    ],
   };
 }
