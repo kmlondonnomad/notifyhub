5211b97eb43bbaa63bf044f707f8d58c33401eb5
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 16:05:59 +0300

    Update notifications.js

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -1,11 +1,12 @@
 import { notificationService } from "../services/notificationService.js";
 import { notificationQueue } from "../queue/notificationQueue.js";
 import { retryService } from "../services/retryService.js";
+import { buildApiResponse } from "../utils/buildApiResponse.js";
 
 export function notificationsRoute() {
-  return {
+  return buildApiResponse({
     service: notificationService(),
     queue: notificationQueue(),
     retry: retryService(),
-  };
+  });
 }
