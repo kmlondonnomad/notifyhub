bf76d090b83bb0143125e78de63041ae8f7a1896
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 09:35:20 +0300

    Update notificationQueue.js

--- src/queue/notificationQueue.js
+++ src/queue/notificationQueue.js
@@ -1,3 +1,7 @@
+import { emailProcessor } from "./processors/emailProcessor.js";
+import { smsProcessor } from "./processors/smsProcessor.js";
+import { pushProcessor } from "./processors/pushProcessor.js";
+
 export function notificationQueue() {
-  return [];
+  return [emailProcessor(), smsProcessor(), pushProcessor()];
 }
