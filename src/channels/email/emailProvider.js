3b71e3776e141987d0c976260137a338a9248bf1
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 16:49:08 +0300

    Update emailProvider.js

--- src/channels/email/emailProvider.js
+++ src/channels/email/emailProvider.js
@@ -1,6 +1,14 @@
 import { logger } from "../../utils/logger.js";
 
-export function sendEmail() {
-  logger("email sent");
-  return "email sent";
+export function sendEmail(input = {}) {
+  const result = {
+    channel: "email",
+    to: input.to || "",
+    subject: input.subject || "",
+    message: input.message || "",
+    status: "sent",
+  };
+
+  logger(result);
+  return result.status;
 }
