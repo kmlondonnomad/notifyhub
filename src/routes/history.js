2a8bf0349dfcdb4b7482cdc1e12f5443b90baee9
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 16:52:54 +0300

    Update history.js

--- src/routes/history.js
+++ src/routes/history.js
@@ -1,6 +1,6 @@
 import { historyService } from "../services/historyService.js";
 import { buildApiResponse } from "../utils/buildApiResponse.js";
 
-export function historyRoute() {
-  return buildApiResponse(historyService());
+export function historyRoute(item = {}) {
+  return buildApiResponse(historyService(item));
 }
