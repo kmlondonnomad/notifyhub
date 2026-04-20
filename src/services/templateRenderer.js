8274b6a5e592b10aabb2c8bace3bd0a6d8d0466f
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 12:05:24 +0300

    Update templateRenderer.js

--- src/services/templateRenderer.js
+++ src/services/templateRenderer.js
@@ -1,6 +1,8 @@
+import { normalizeTemplateName } from "../utils/normalizeTemplateName.js";
+
 export function templateRenderer(templateName, data) {
   return {
-    templateName,
+    templateName: normalizeTemplateName(templateName),
     data,
   };
 }
