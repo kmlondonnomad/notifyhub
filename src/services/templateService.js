1538c36d1d9352951dc37c4a2ba17a842aeffc07
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 10:02:45 +0300

    Update templateService.js

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -1,5 +1,8 @@
-export function templateService() {
+import { buildTemplateData } from "../utils/buildTemplateData.js";
+
+export function templateService(data = {}) {
   return {
     templates: ["welcome", "alert", "status"],
+    data: buildTemplateData(data),
   };
 }
