d41c5830591644c73f2842539e98aa9733cbf28b
kmlondonnomad <kemalabn21@gmail.com>
2026-04-20 16:47:52 +0300

    Update pushProcessor.js

--- src/queue/processors/pushProcessor.js
+++ src/queue/processors/pushProcessor.js
@@ -1,5 +1,8 @@
 import { sendPush } from "../../channels/push/fcmProvider.js";
 
-export function pushProcessor() {
-  return sendPush();
+export function pushProcessor(notification = {}) {
+  return sendPush({
+    token: "demo-device-token",
+    message: notification.payload?.message || "",
+  });
 }
